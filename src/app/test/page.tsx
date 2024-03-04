f2aa7488357111ec682d9aae43014ddab3e94e9b
neilck <neil@chong-kit.com>
2024-03-04 14:53:44 -0800

    updated test page

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 import { Suspense } from "react";
+import { verifySession } from "../actions/akaActions";
 const ShowSearchParams = () => {
   const searchParams = useSearchParams();
   const id = searchParams.get("id");
@@ -14,12 +16,35 @@ const ShowSearchParams = () => {
 };
 
 export default function Home() {
+  const [isValidSession, setIsValidSession] = useState(false);
+
+  useEffect(() => {
+    checkSession();
+  }, []);
+
+  // verifying session lets us know AKA Profiles is making the request
+  const checkSession = async () => {
+    const session = "Rg7wDuOnYoOhRuGFUrPL";
+    const awardtoken = "aTc3qHMUdUyzrDVMhHR8kc";
+
+    const result = await verifySession(session, awardtoken).catch((error) => {
+      console.log(error);
+      return;
+    });
+
+    console.log(result);
+    if (result) {
+      setIsValidSession(result.success);
+    }
+  };
+
   return (
     <>
       Test Page
       <Suspense>
         <ShowSearchParams />
       </Suspense>
+      <p>isValidSession: {isValidSession.toString()}</p>
     </>
   );
 }
