4c02fe395da9d3c12fcb528bea9857fd2e9eab5e
neilck <neil@chong-kit.com>
2024-03-04 23:00:33 -0800

    changed Access Control to all paths

--- next.config.js
+++ next.config.js
@@ -11,8 +11,8 @@ const nextConfig = {
   async headers() {
     return [
       {
-        // matching all API routes
-        source: "/api/:path*",
+        // matching all  routes
+        source: "/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
