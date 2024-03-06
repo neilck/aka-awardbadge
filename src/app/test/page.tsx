2873a0b0fc135aa58ea3852f14c96dc13cf939c4
neilck <neil@chong-kit.com>
2024-03-05 21:33:08 -0800

    udpated test

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,38 +1,22 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { create, greaterThan10 } from "./actions";
-export default function Page() {
-  const [value, setValue] = useState(1);
-  const [isGreater, setIsGreater] = useState(false);
+import { sayHello } from "./actions";
 
-  /*
+export default function Page() {
+  const [message, SetMessage] = useState("goodbye");
+
   useEffect(() => {
-    const doSomething = async () => {
-      const isGreater = await greaterThan10(value);
-      setIsGreater(isGreater);
+    const callSayHello = async () => {
+      const mesg = await sayHello();
+      SetMessage(mesg);
     };
 
-    doSomething();
-  }, [value]);
-  */
-
+    callSayHello();
+  });
   return (
     <>
-      <form action={create}>
-        <input type="text" name="name" />
-        <button type="submit">Submit</button>
-      </form>
-      <button
-        onClick={() => {
-          if (isGreater) setValue(10);
-          else setValue(100);
-        }}
-      >
-        Set to 100
-      </button>
-      <p>{value}</p>
-      <p>{isGreater.toString()}</p>
+      <p>{message}</p>
     </>
   );
 }
